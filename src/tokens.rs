91f53ffb941b32e1ab46156d091c7517c62b6e70
Jakub Duda <121130603+Dudu7352@users.noreply.github.com>
2024-08-03 20:11:49 +0200

    tokens

---
+++ src/tokens.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+pub enum Token {
+    Op(Operator),
+    Number(f32),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Operator {
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    LParen,
+    RParen,
+}
+
+impl Operator {
+    fn weight(&self) -> u8 {
+        match self {
+            Operator::Plus => 1,
+            Operator::Minus => 1,
+            Operator::Star => 2,
+            Operator::Slash => 2,
+            Operator::LParen => 0,
+            Operator::RParen => 0,
+        }
+    }
+}
+
+impl PartialOrd for Operator {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        return Some(self.weight().cmp(&other.weight()));
+    }
+}
